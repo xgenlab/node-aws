28de25acc3fb6541367ddf67f5a02d298fbfde2f
samadhi laksahan <samadhilaksahan007@gmail.com>
2022-01-31 23:36:15 +0530

    add code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res)=>{
     res.send("welcome to the home page");
 })
 
-const port = process.env.port || 3000 ;
+const port = process.env.PORT || 3000 ;
 app.listen( port , ()=>{
     console.log('app start to work at port', port);
 });
